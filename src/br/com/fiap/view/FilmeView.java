8b8092827b8dc6b6c8735a54b3ca1f206c9372d0
JVPR <jvprocha.05@gmail.com>
2026-09-23 18:09:51 -0300

    Create View e FilmeView

--- src/br/com/fiap/view/FilmeView.java
+++ src/br/com/fiap/view/FilmeView.java
@@ -1,4 +1,11 @@
+// Guilherme de Araujo Moreira RM: 561848
+// Joao Vitor Parizotto Rocha RM: 562719
+// Leonardo Silva Pinto RM: 564929
+// Samuel Enzo RM: 564391
 package br.com.fiap.view;
 
 public class FilmeView {
+    static void main() {
+
+    }
 }
