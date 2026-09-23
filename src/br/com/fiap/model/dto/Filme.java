675a124254bcb894e763e79e1971dcde1a04a3f0
JVPR <jvprocha.05@gmail.com>
2026-09-23 17:46:52 -0300

    Adicionando getters e setters

--- src/br/com/fiap/model/dto/Filme.java
+++ src/br/com/fiap/model/dto/Filme.java
@@ -9,4 +9,29 @@ public class Filme {
     private String titulo;
     private String genero;
     private String produtora;
+
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    public String getProdutora() {
+        return produtora;
+    }
+    public void setProdutora(String produtora) {
+        this.produtora = produtora;
+    }
 }
