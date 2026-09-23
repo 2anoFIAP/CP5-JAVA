42a08efc9faa76b00b11ba2eba457af991e1c3ac
JVPR <jvprocha.05@gmail.com>
2026-09-23 17:48:20 -0300

    iniciando Controller

--- src/br/com/fiap/controller/FilmeController.java
+++ src/br/com/fiap/controller/FilmeController.java
@@ -4,10 +4,17 @@
 // Samuel Enzo RM: 564391
 package br.com.fiap.controller;
 
+import br.com.fiap.model.dao.ConnectionFactory;
+import br.com.fiap.model.dto.Filme;
+
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class FilmeController {
-    public String inserirFilme(String titulo, String genero, String produtora){
+    public String inserirFilme(String titulo, String genero, String produtora) throws ClassNotFoundException, SQLException {
+        String resultado;
+        Connection con = ConnectionFactory.abrirConexao();
+        Filme filme = new Filme();
 
     }
 
